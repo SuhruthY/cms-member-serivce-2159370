169e21915d725d04570e91638e5c91e44030ded1
SuhruthY <suhruthyambakam822001@gmail.com>
2022-08-11 22:25:11 +0530

    changed some files

--- src/test/java/com/cts/MemberMicroservice/MemberMicroserviceApplicationTests.java
+++ src/test/java/com/cts/MemberMicroservice/MemberMicroserviceApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MemberMicroserviceApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+		assert(true);
+	}
 
 }
